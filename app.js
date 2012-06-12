dd3c32840eeb520a5b1630a917fca78bd198157e
Jared Greene <jegreene@gmail.com>
2012-06-12 13:38:34 -0400

    All Stations are now working ... some repeats in list will be fixed in next iteration
    WMATA API has multiple train codes for the same train line so code needs
    to be changed to determine if train line has already been added

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var Request = require("request");
+var Request = require('request');
 var Log = require('logging');
 var Async = require('async');
 
 var stations = {};
 var api_key = '67syqrmbw6gz6escepcmuvnv';
 var metro = ['RD','BL', 'GR', 'YL', 'OR'];  //Current Line code for metro;
-
+var allTrains = { redLine: [ ], blueLine: [ ], greenLine: [ ], yellowLine: [ ], orangeLine: [ ] };
 var toFind = (process.argv[2] + (process.argv[3] ? " " + process.argv[3] : "")).toLowerCase();
+
 
 Async.forEach(metro,function(lineCode, cb){
     Request('http://api.wmata.com/Rail.svc/json/JStations?LineCode='+lineCode+'&api_key=' + api_key, function (error, response, body) {
@@ -15,31 +16,69 @@ Async.forEach(metro,function(lineCode, cb){
          //   Log(d.Stations);
             d.Stations.forEach(function(Station){
             //  Log(Station.Name, Station.Code);
-               stations[Station.Name.toLowerCase()] = Station.Code;
+                var stationName = Station.Name.toLowerCase();
+                if (stations[stationName] == undefined){
+                    stations[stationName] = new Array(Station.Code);
+                }
+                else{
+                    stations[stationName].push(Station.Code);
+                }
+
             });
 
-          //  Log(stations);
+            //Log(stations);
 
         }
+        //Log(stations);
         cb(error);
+
     });
 
 }, function(error){
     //Log(stations);
+   // Log(stationCodes);
     var toSearch = stations[toFind];
-    Log(toSearch);
+    //Log(toSearch);
     Request('http://api.wmata.com/StationPrediction.svc/json/GetPrediction/'+toSearch+'?api_key=' + api_key, function(error, response, body){
     if (!error && response.statusCode == 200){
         var d = JSON.parse(body);
-      if(!d.Trains.length)
+      //Log(d);
+      if (toSearch == undefined){
+          console.log('Not a train station. Please use correct name!');
+      }
+      else if(!d.Trains.length)
       {
-         Log("No Trains Yet!");
+         console.log('No Trains Reported Yet!');
       }
       else {
           d.Trains.forEach(function(Train){
-              console.log('Direction : ' +Train.Destination + ' Time: ' +Train.Min);
+              var to_add = 'Direction : ' +Train.Destination + ' Time: ' +Train.Min;
+              //Log(Train.Line);
+             switch(Train.Line)
+              {
+                 case 'GR':
+                     allTrains.greenLine.push(to_add);
+                     break;
+                 case 'YL':
+                     allTrains.yellowLine.push(to_add);
+                     break;
+                 case 'RD':
+                     allTrains.redLine.push(to_add);
+                     break;
+                 case 'OR':
+                     allTrains.orangeLine.push(to_add);
+                     break;
+                 case 'BL':
+                     allTrains.blueLine.push(to_add);
+                     break;
+                 default:
+                     //Log("Should not have gotten here!!");
+              }
+
 
           });
+          console.log(allTrains);
+
       }
 
 
@@ -47,6 +86,7 @@ Async.forEach(metro,function(lineCode, cb){
 
 
      });
+
 
 });
 
